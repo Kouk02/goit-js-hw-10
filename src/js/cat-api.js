3c7a9d31d006d3d0cb52deedffd49d71a570c0d5
Kouk02 <kouk20020703@gmail.com>
2023-11-03 15:35:36 +0200

    fix task-10 (export)

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_jCOpu4zU55giJc6i7DERf2RBmCfqkdcwpGNjBKcQvgtrEt9UVDlsxnOMGSYmJRKq";
 
 // Функція для відправки запиту на отримання інформації про кота за ідентифікатором породи
-function fetchCatByBreed(breedId) {
+export function fetchCatByBreed(breedId) {
   return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
     .then((response) => {
       const cat = response.data[0];
